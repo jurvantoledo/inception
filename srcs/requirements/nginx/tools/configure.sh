05017ff5b276619c71e4e1815de09a43a4ae8feb
Jur Van toledo <jvan-tol@f1r5s5.codam.nl>
2023-06-08 14:51:57 +0200

    poes

--- srcs/requirements/nginx/tools/configure.sh
+++ srcs/requirements/nginx/tools/configure.sh
@@ -2,19 +2,20 @@
 
 sed -i "s/SERVER_NAME/$DOMAIN_NAME/g" /etc/nginx/sites-enabled/nginx.conf
 
-# if [ ! -f "/etc/ssl/certs/certificate.crt" ]; then
-# 	echo "Making the certificate"
+if [ ! -f "/etc/ssl/certs/certificate.crt" ]; then
+	echo "Making the certificate"
 
-# 	openssl req -new \
-# 		-newkey rsa:2048 \
-# 		-x509 \
-# 		-sha256 \
-# 		-days 365 \
-# 		-nodes \
-# 		-out /etc/ssl/certs/certificate.crt \
-# 		-keyout /etc/ssl/private/certificate.key \
-# 		-subj "/C=$SSL_COUNTRY/ST=$SSL_STATE/L=$SSL_CITY/O=$SSL_ORGANISATION/CN=$DOMAIN_NAME"
-# else
-# 	echo "Certificate already created"
-# fi
+	# Creating the SSL certificate
+	openssl req -new \
+		-newkey rsa:2048 \
+		-x509 \
+		-sha256 \
+		-days 365 \
+		-nodes \
+		-out /etc/ssl/certs/certificate.crt \
+		-keyout /etc/ssl/private/certificate.key \
+		-subj "/C=$SSL_COUNTRY/ST=$SSL_STATE/L=$SSL_CITY/O=$SSL_ORGANISATION/CN=$DOMAIN_NAME"
+else
+	echo "Certificate already created"
+fi
 exec "$@"
